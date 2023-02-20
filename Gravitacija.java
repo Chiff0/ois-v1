8a33b045cf9a5b4c75e17cef322e6036d3815da6
Chiff0 <fd92427@student.uni-lj.si>
2023-02-20 15:42:54 +0100

    Lepse

--- Gravitacija.java
+++ Gravitacija.java
@@ -10,9 +10,8 @@ public class Gravitacija {
         double R = 6371000;
         double g = C * M / Math.pow(R + h, 2);
 
-        System.out.println("Podana visina je " + h + " m");
-
-        System.out.println("Gravitacijska pospešek na površju Zemlje je " + g + " m/s^2");
+        System.out.println("Višina je " + h + " m.");
+        System.out.println("Gravitacijska pospešek na površju Zemlje je " + g + " m/s^2.");
 
     }
 }
